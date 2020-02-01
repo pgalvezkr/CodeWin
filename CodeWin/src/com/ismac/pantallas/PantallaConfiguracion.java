4476bd3a4160c40f97da55f483cd4bde72353bc6
Marjorie <alexa.portilla.1999@gmail.com>
2020-02-01 13:43:03 -0500

    MM:Creacion de Pantallas

---
+++ CodeWin/src/com/ismac/pantallas/PantallaConfiguracion.java
@@ -0,0 +1,56 @@
+package com.ismac.pantallas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+public class PantallaConfiguracion {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PantallaConfiguracion window = new PantallaConfiguracion();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PantallaConfiguracion() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 434, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 261, Short.MAX_VALUE)
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+}
--- CodeWin/src/com/ismac/pantallas/Principal.java
+++ CodeWin/src/com/ismac/pantallas/Principal.java
@@ -1,5 +1,88 @@
 package com.ismac.pantallas;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Canvas;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+
 public class Principal {
+
+	private JFrame frmPrincipal;
+	/**
+	 * @wbp.nonvisual location=218,99
+	 */
+	private final Canvas canvas = new Canvas();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal window = new Principal();
+					window.frmPrincipal.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Principal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmPrincipal = new JFrame();
+		frmPrincipal.getContentPane().setFont(new Font("Segoe Print", Font.BOLD, 11));
+		frmPrincipal.setTitle("Principal");
+		frmPrincipal.setBounds(100, 100, 450, 300);
+		frmPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnLector = new JButton("Leer de Codigo");
+		btnLector.setFont(new Font("Book Antiqua", Font.ITALIC, 13));
+		
+		JButton btnConfiguracion = new JButton("Configuracion");
+		btnConfiguracion.setFont(new Font("Book Antiqua", Font.ITALIC, 13));
+		
+		JButton btnReporte = new JButton("Reportes");
+		btnReporte.setFont(new Font("Book Antiqua", Font.ITALIC, 13));
+		GroupLayout groupLayout = new GroupLayout(frmPrincipal.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(27)
+					.addComponent(btnLector)
+					.addGap(39)
+					.addComponent(btnConfiguracion)
+					.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+					.addComponent(btnReporte, GroupLayout.PREFERRED_SIZE, 95, GroupLayout.PREFERRED_SIZE)
+					.addGap(19))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(175, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnLector)
+						.addComponent(btnConfiguracion)
+						.addComponent(btnReporte))
+					.addGap(63))
+		);
+		frmPrincipal.getContentPane().setLayout(groupLayout);
+	}
 
 }
