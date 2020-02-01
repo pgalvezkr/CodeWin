9134196cb784c41c992d84797c2d094262f9abeb
vero-garcia <gveronica257@yahoo.com>
2020-02-01 13:02:46 -0500

    VG: creacion de clases y atributos
    se crearon las clases en  atributos en el paquete intedada

---
+++ CodeWin/src/com/ismac/entidades/Grupo.java
@@ -0,0 +1,32 @@
+package com.ismac.entidades;
+
+public class Grupo {
+	private int IdGrupo;
+	private int PuntosObtenidos;
+	private int NumeroPremios;
+
+	public int getNumeroPremios() {
+		return NumeroPremios;
+	}
+
+	public void setNumeroPremios(int numeroPremios) {
+		NumeroPremios = numeroPremios;
+	}
+
+	public int getIdGrupo() {
+		return IdGrupo;
+	}
+
+	public void setIdGrupo(int idGrupo) {
+		IdGrupo = idGrupo;
+	}
+
+	public int getPuntosObtenidos() {
+		return PuntosObtenidos;
+	}
+
+	public void setPuntosObtenidos(int puntosObtenidos) {
+		PuntosObtenidos = puntosObtenidos;
+	}
+
+}
