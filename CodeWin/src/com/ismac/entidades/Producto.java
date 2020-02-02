7c45c276f9ccbdb5f2ab72d507d505f0a647ad9b
juankgp <juank@juank-PC>
2020-02-01 19:43:11 -0500

    JG cambios en clase Producto

--- CodeWin/src/com/ismac/entidades/Producto.java
+++ CodeWin/src/com/ismac/entidades/Producto.java
@@ -1,31 +1,28 @@
 package com.ismac.entidades;
 
 public class Producto {
-	private int IdProducto;
-	private int Nombre;
-	private int Precio;
-
+	private int idProducto;
+	private String nombre;
+	private int precio;
 	public int getIdProducto() {
-		return IdProducto;
+		return idProducto;
 	}
-
 	public void setIdProducto(int idProducto) {
-		IdProducto = idProducto;
+		this.idProducto = idProducto;
 	}
-
-	public int getNombre() {
-		return Nombre;
+	public String getNombre() {
+		return nombre;
 	}
-
-	public void setNombre(int nombre) {
-		Nombre = nombre;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-
 	public int getPrecio() {
-		return Precio;
+		return precio;
 	}
-
 	public void setPrecio(int precio) {
-		Precio = precio;
+		this.precio = precio;
 	}
+	
 }
+
+
---
+++ CodeWin/src/com/ismac/servicios/test/TestEntidades.java
@@ -0,0 +1,13 @@
+package com.ismac.servicios.test;
+
+import com.ismac.entidades.Producto;
+
+public class TestEntidades {
+
+	public static void main(String[] args) {
+			Producto producto = new Producto();
+			producto.setNombre("Peras");
+
+	}
+
+}
