dbdbd8acc5d0a9205f9eae023600bd7b77981ce6
juankgp <jcfenix01@gmail.com>
2020-02-02 00:04:04 -0500

    JG clase TestEntidades y calse ManejoSqlpara conexion a base de datos

--- CodeWin/src/com/ismac/servicios/test/TestEntidades.java
+++ CodeWin/src/com/ismac/servicios/test/TestEntidades.java
@@ -1,14 +1,18 @@
 package com.ismac.servicios.test;
 
+import com.ismac.entidades.ManejoSql;
 import com.ismac.entidades.Producto;
 
 public class TestEntidades {
 
 	public static void main(String[] args) {
 			Producto producto = new Producto();
+			ManejoSql conexion = new ManejoSql();
 			producto.setNombre("Peras");
 			producto.setPrecio(10);
-			
+			String insertSql = "INSERT INTO Products1 (Nombre, Precio) VALUES "
+			        + "('"+producto.getNombre()+"',"+ producto.getPrecio()+");";
+			conexion.insertSql(insertSql);
 			
 
 	}
