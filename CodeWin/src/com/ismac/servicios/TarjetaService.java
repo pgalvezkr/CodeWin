e55fd9490170e33a319e283fc8ef30bdab4f6380
leonardog2018 <leito_g2010@hotmail.com>
2020-02-01 20:33:57 -0500

    LG continuacion Servicios
    Se creaa codigo para leer targeta

--- CodeWin/src/com/ismac/servicios/TarjetaService.java
+++ CodeWin/src/com/ismac/servicios/TarjetaService.java
@@ -1,5 +1,11 @@
 package com.ismac.servicios;
 
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class TarjetaService {
 
 
@@ -16,7 +22,33 @@ public class TarjetaService {
 
 		try (Connection connection = DriverManager.getConnection(connectionUrl);
 				PreparedStatement prepsInsertProduct = connection.prepareStatement(sentenceSql,
-						Statement.RETURN_GENERATED_KEYS);) {
+						Statement.RETURN_GENERATED_KEYS())); {
+			prepsInsertProduct.execute();
+			
+			resultSet = prepsInsertProduct.getGeneratedKeys();
+
+			while (resultSet.next()) {
+				System.out.println("Generado: " + resultSet.getString(1));
+			}
+		}
+		// Handle any errors that may have occurred.////////
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return resultSet;
+
+	// LEER TARJETA
+	public ResultSet leer (int idTarjeta, int tipoTarjeta, int estacion, int valorTarjeta) {
+
+		ResultSet resultSet2 = null;
+
+		// SENTENCIA SQL
+		String sentenceSql2 = "INSERT INTO Tarjeta (IdTarjeta, TipoTarjeta, Estacion, ValorTarjeta) VALUES (" + idTarjeta
+				+ ", " + tipoTarjeta + ", " + estacion + ", " + valorTarjeta + ")";
+
+		try (Connection connection = DriverManager.getConnection(connectionUrl);
+				PreparedStatement prepsInsertProduct = connection.prepareStatement(sentenceSql,
+						Statement.RETURN_GENERATED_KEYS())); {
 			prepsInsertProduct.execute();
 			
 			resultSet = prepsInsertProduct.getGeneratedKeys();
@@ -32,5 +64,7 @@ public class TarjetaService {
 		return resultSet;
 	}
 
+	
+	}
 	
 }
