e7267c4c753055726d299357acfcc30353e2638a
leonardog2018 <44074360+leonardog2018@users.noreply.github.com>
2020-02-03 21:29:38 -0500

    LG
    SE CREA DENTRO DE LOS SERVICIOS

--- CodeWin/src/com/ismac/controller/ProductoController.java
+++ CodeWin/src/com/ismac/controller/ProductoController.java
@@ -1,5 +1,32 @@
 package com.ismac.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ismac.entidades.Producto;
+import com.ismac.entidades.Tarjeta;
+import com.ismac.servicios.ProductoService;
+import com.ismac.servicios.TarjetaService;
+
 public class ProductoController {
 
+	public static List<Tarjeta> getProducto() {
+		List<Tarjeta> productos = new ArrayList<>();
+		try {
+			Object tarjetas = ProductoService.listar();
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al consultar las tarjetas " + e.getMessage());
+			e.printStackTrace();
+		}
+		return productos;
+	}
+
+	public static void crearTarjeta(Tarjeta tarjeta) {
+		try {
+			TarjetaService.crear(tarjeta.getTipoDeTarjeta(), tarjeta.getEstacion(), tarjeta.getValorDeTarjeta());
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al guardar las tarjeta " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
--- CodeWin/src/com/ismac/controller/StockController.java
+++ CodeWin/src/com/ismac/controller/StockController.java
@@ -1,5 +1,30 @@
 package com.ismac.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ismac.entidades.Stock;
+import com.ismac.servicios.StockService;
+
 public class StockController {
 
+	public static List<Stock> getStock() {
+		List<Stock> stock = new ArrayList<>();
+		try {
+			stock = StockService.listar();
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al consultar las tarjetas " + e.getMessage());
+			e.printStackTrace();
+		}
+		return stock;
+	}
+
+	public static void stock(Stock stock) {
+		try {
+			Service.crear(tarjeta.getTipoDeTarjeta(), tarjeta.getEstacion(), tarjeta.getValorDeTarjeta());
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al guardar las tarjeta " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
