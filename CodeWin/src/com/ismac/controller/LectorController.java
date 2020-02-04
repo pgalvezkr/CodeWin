afc1058e4157c517d65222d56faec26f91907932
leonardog2018 <44074360+leonardog2018@users.noreply.github.com>
2020-02-03 21:26:48 -0500

    LG controladores
    En los controladores se genera para las clases

--- CodeWin/src/com/ismac/controller/LectorController.java
+++ CodeWin/src/com/ismac/controller/LectorController.java
@@ -2,6 +2,31 @@ package com.ismac.controller;
 
 import java.awt.Image;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ismac.entidades.Tarjeta;
+import com.ismac.servicios.TarjetaService;
+
 public class LectorController {
 
+	public static List<Tarjeta> getTarjetas() {
+		List<Tarjeta> tarjetas = new ArrayList<>();
+		try {
+			tarjetas = TarjetaService.listar();
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al consultar las tarjetas " + e.getMessage());
+			e.printStackTrace();
+		}
+		return tarjetas;
+	}
+
+	public static void crearTarjeta(Tarjeta tarjeta) {
+		try {
+			TarjetaService.crear(tarjeta.getTipoDeTarjeta(), tarjeta.getEstacion(), tarjeta.getValorDeTarjeta());
+		} catch (Exception e) {
+			System.out.println("Ocurrió un error al guardar las tarjeta " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
 }
